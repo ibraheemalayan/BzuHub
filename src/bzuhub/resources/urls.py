a97e75feadcbd113ef2766638765932f76b3d66e
Ibraheem Alyan <35265448+ibraheemalayan@users.noreply.github.com>
2023-12-11 13:54:51 +0200

    feat: ✨ simple bootstrap templates to browse colleges and view departments/majors

--- src/bzuhub/resources/urls.py
+++ src/bzuhub/resources/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("colleges/", views.college_list, name="college_list"),
+    path("colleges/<int:college_id>/", views.college_page, name="college_page"),
 ]
--- src/bzuhub/resources/views.py
+++ src/bzuhub/resources/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import College
 
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the resources index.")
+    return render(request, "resources/base.html")
+
+
+def college_list(request):
+    colleges = College.objects.all()
+    return render(request, "resources/college_list.html", {"colleges": colleges})
+
+
+def college_page(request, college_id):
+    college = College.objects.get(id=college_id)
+
+    departments = college.departments.all()
+
+    return render(
+        request,
+        "resources/college_page.html",
+        {"college": college, "departments": departments},
+    )
